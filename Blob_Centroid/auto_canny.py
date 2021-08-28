9b1415b403a2f862073e04f854c4e08284da908f
Akshansh <qaz.akshansh@gmail.com>
2021-08-28 13:36:20 +0530

    Kindof Works

---
+++ Blob_Centroid/auto_canny.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+def auto_canny(image, sigma=0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	# return the edged image
+	return cv2.Canny(image, lower, upper)
+
+def thresh_optima(img):
+    high_thresh, thresh_im = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    low_thresh = 0.5*high_thresh
+    return low_thresh, high_thresh
